31fcbe4b882f7bbcc3da80c00b176978cce1ea90
EricLBuehler <ericlbuehler@gmail.com>
2023-04-24 16:48:54 +0000

    Update setup shell

--- .ipynb_checkpoints/setup-checkpoint.sh
+++ .ipynb_checkpoints/setup-checkpoint.sh
@@ -2,7 +2,7 @@ sudo apt-get install git-lfs
 git clone https://github.com/EricLBuehler/ScienceGPT.git
 cd ScienceGPT
 git lfs install
-git lfs track "models"
+git lfs track "*.pt"
 git config --global credential.helper store
 git config --global user.email "ericlbuehler@gmail.com"
 git config --global user.name "EricLBuehler"
