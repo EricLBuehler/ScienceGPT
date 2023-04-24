124881b385fef84bdea62cec5ddcca4120f77233
EricLBuehler <ericlbuehler@gmail.com>
2023-04-24 16:24:51 +0000

    Setup with new dataset

--- dataset_prep/scraper.py
+++ dataset_prep/scraper.py
@@ -4,12 +4,12 @@ import tqdm
 def clean_text(text: str) -> str: #Strip text
     return text.strip()
 
-def scrape_subreddit(reddit: praw.Reddit, data: list, subreddit: str, skip: int, max_allow_len: int = 2048) -> int:
+def scrape_subreddit(reddit: praw.Reddit, data: list, subreddit: str, max_allow_len: int = 1024) -> int:
     print("Scraping", subreddit)
     hot_posts = reddit.subreddit(subreddit).hot(limit = 10000)
     maxlen = -1
-    for i, post in tqdm.tqdm(enumerate(hot_posts)):
-        if i < skip: # Skip headers
+    for post in tqdm.tqdm(hot_posts):
+        if post.stickied or post.pinned: # Skip headers
             continue
         question = ""
         if post.title[-1] not in [".", "!", "?"]:
@@ -20,13 +20,14 @@ def scrape_subreddit(reddit: praw.Reddit, data: list, subreddit: str, skip: int,
         question += " "
         question += clean_text(post.selftext)
         question += "\n"
+        
         for top_level_comment in reddit.submission(id=post.id).comments:
             if isinstance(top_level_comment, praw.models.MoreComments):
                 for comment in top_level_comment.comments():
                     if comment.body == "[removed]" or comment.stickied:
                         continue
                     res = question + clean_text(comment.body) + "\0"
-                    if len(res) > max_allow_len:
+                    if len(list(bytes(res, "utf8"))) > max_allow_len:
                         continue
                     data.append(res)
                     if len(data[-1]) > maxlen:
@@ -35,9 +36,8 @@ def scrape_subreddit(reddit: praw.Reddit, data: list, subreddit: str, skip: int,
 
             if top_level_comment.body == "[removed]" or top_level_comment.stickied:
                 continue
-                    
             res = question + clean_text(top_level_comment.body) + "\0"
-            if len(res) > max_allow_len:
+            if len(list(bytes(res, "utf8"))) > max_allow_len:
                 continue
             data.append(res)
             
@@ -51,23 +51,39 @@ reddit = praw.Reddit(client_id="fO05Zgvq53-yoAPrz-Yesw", client_secret='9rkeMCec
 data = []
 maxlen = -1
 
-max = scrape_subreddit(reddit, data, "AskScience", 2) 
+max = scrape_subreddit(reddit, data, "AskScience") 
 if max > maxlen:
     maxlen = max
 
-max = scrape_subreddit(reddit, data, "LearnPython", 1) 
+max = scrape_subreddit(reddit, data, "LearnProgramming") 
 if max > maxlen:
     maxlen = max
 
-max = scrape_subreddit(reddit, data, "cpp_questions", 2) 
+max = scrape_subreddit(reddit, data, "LearnPython") 
 if max > maxlen:
     maxlen = max
 
-max = scrape_subreddit(reddit, data, "javahelp", 1) 
+max = scrape_subreddit(reddit, data, "cpp_questions") 
 if max > maxlen:
     maxlen = max
 
-max = scrape_subreddit(reddit, data, "askprogramming", 1) 
+max = scrape_subreddit(reddit, data, "javahelp") 
+if max > maxlen:
+    maxlen = max
+
+max = scrape_subreddit(reddit, data, "askprogramming") 
+if max > maxlen:
+    maxlen = max
+
+max = scrape_subreddit(reddit, data, "java") 
+if max > maxlen:
+    maxlen = max
+
+max = scrape_subreddit(reddit, data, "CodingHelp") 
+if max > maxlen:
+    maxlen = max
+
+max = scrape_subreddit(reddit, data, "AskProgrammers") 
 if max > maxlen:
     maxlen = max
 
