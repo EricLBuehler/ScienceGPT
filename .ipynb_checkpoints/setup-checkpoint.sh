8abf8ff24ad06ec493431f6eff2ae461d1c951b3
EricLBuehler <ericlbuehler@gmail.com>
2023-04-24 10:27:20 +0000

    Update and setup

--- .ipynb_checkpoints/setup-checkpoint.sh
+++ .ipynb_checkpoints/setup-checkpoint.sh
@@ -1,8 +1,6 @@
-sudo apt-get update
-sudo apt-get upgrade
 sudo apt-get install git-lfs
+git clone https://github.com/EricLBuehler/ScienceGPT.git
+cd ScienceGPT
 git lfs install
 git lfs track "models"
-git clone https://github.com/EricLBuehler/MathGPT.git
-cd MathGPT
 git config --global credential.helper store
--- setup.sh
+++ setup.sh
@@ -1,8 +1,6 @@
-sudo apt-get update
-sudo apt-get upgrade
 sudo apt-get install git-lfs
-git lfs install
-git lfs track "models"
 git clone https://github.com/EricLBuehler/ScienceGPT.git
 cd ScienceGPT
+git lfs install
+git lfs track "models"
 git config --global credential.helper store
